47323b3d5dd4b295e9042e4836f20b30435894f7
Jerel Hass <agies1@hotmail.com>
2013-05-22 19:00:50 -0400

    Minor changes and adding in the module

---
+++ Modules/Orchard.PopForumsAdapter/DataMigrations/Migrations.cs
@@ -0,0 +1,13 @@
+﻿using Orchard.Data.Migration;
+
+namespace Orchard.PopForumsAdapter.DataMigrations
+{
+    public class Migrations : DataMigrationImpl
+    {
+         public int Create()
+         {
+
+             return 1;
+         }
+    }
+}
---
+++ Modules/Orchard.PopForumsAdapter/Models/ForumRecord.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Orchard.ContentManagement;
+using Orchard.ContentManagement.Records;
+
+namespace Orchard.PopForumsAdapter.Models
+{
+    public class ForumPartRecord : ContentPartRecord
+    {
+        public virtual int ForumID { get; set; }
+        public virtual int? CategoryID { get; set; }
+        public virtual string Title { get; set; }
+        public virtual string Description { get; set; }
+        public virtual bool IsVisible { get; set; }
+        public virtual bool IsArchived { get; set; }
+        public virtual int SortOrder { get; set; }
+        public virtual int TopicCount { get; set; }
+        public virtual int PostCount { get; set; }
+        public virtual DateTime LastPostTime { get; set; }
+        public virtual string LastPostName { get; set; }
+        public virtual string UrlName { get; set; }
+        public virtual string ForumAdapterName { get; set; }
+    }
+
+    public class ForumPart : ContentPart<ForumPartRecord>
+    {
+        public int? CategoryID
+        {
+            get { return Record.CategoryID; }
+            set { Record.CategoryID = value; }
+        }
+
+        public string Title
+        {
+            get { return Record.Title; }
+            set { Record.Title = value; }
+        }
+
+        public string Description
+        {
+            get { return Record.Description; }
+            set { Record.Description = value; }
+        }
+
+        public bool IsVisible
+        {
+            get { return Record.IsVisible; }
+            set { Record.IsVisible = value; }
+        }
+    }
+}
